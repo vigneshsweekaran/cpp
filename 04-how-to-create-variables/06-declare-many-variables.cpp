b38607289ba8c3040688724a109a36b6d7741070
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-09-28 07:21:28 +0530

    update

---
+++ 04-how-to-create-variables/06-declare-many-variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5, y = 6, z = 50;
+  cout << "the value of x : " << x;
+  cout << "the value of y : " << y;
+  cout << "the value of z : " << z;
+  cout << "The sum of 3 number : " << x + y + z;
+  return 0;
+}
